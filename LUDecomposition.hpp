729806cf629b78432d92ed6df1737a2fdef69120
Kirill Shepelev <ntngel1@gmail.com>
2018-07-13 23:33:33 +0300

    Written docs, created getters

--- LUDecomposition.hpp
+++ LUDecomposition.hpp
@@ -13,24 +13,65 @@
 namespace MatrixCpp {
 
 /**
- * @brief Struct to work with decomposition of matrix
+ * @brief Class to work with decomposition of matrix
  * 
  * @tparam Type of square matrice's elements
  */
 template<class T>
-struct LUDecomposition {
+class LUDecomposition {
+public:
     /**
-     * @brief Size of the matrix (as matrix is square, this size is rows' size and columns' size)
+     * @brief Construct a new empty LUDecomposition object
      * 
      */
-    std::size_t size;
+    LUDecomposition();
 
     /**
-     * @brief Sets to true, if decomposition wasn't gotten
+     * @brief Construct a new LUDecomposition object with decomposition of given matrix
      * 
+     * @param Matrix to get decomposition
      */
-	bool isEmpty;
-    
+	LUDecomposition(const Matrix<T>& matrix);
+
+    /**
+     * @brief Decomposes given matrix
+     * 
+     * @param Matrix to get decomposition
+     * @return true if decomposition was successfully gotten
+     * @return false if decomposition wasn't gotten
+     */
+    bool decompose(const Matrix<T>& matrix);
+
+    /**
+     * @brief Get shared_ptr to L-matrix of decomposition
+     * 
+     * @return std::shared_ptr<Matrix<T>>
+     */
+    std::shared_ptr<Matrix<T>> getL() const;
+
+    /**
+     * @brief Get shared_ptr to U-matrix of decomposition
+     * 
+     * @return std::shared_ptr<Matrix<T>> 
+     */
+    std::shared_ptr<Matrix<T>> getU() const;
+
+    /**
+     * @brief Get the size of L and U matrices (Size x Size)
+     * 
+     * @return std::size_t 
+     */
+    std::size_t getSize() const;
+
+    /**
+     * @brief Check is decomposition empty or not (empty when given matrix isn't square)
+     * 
+     * @return true if decomposition is empty
+     * @return false if decomposition isn't empty
+     */
+    bool isEmpty() const;
+
+private:
     /**
      * @brief Shared-ptr to L-matrix of decomposition
      * 
@@ -44,78 +85,97 @@ struct LUDecomposition {
 	std::shared_ptr<Matrix<T>> U;
 
     /**
-     * @brief Construct a new empty LUDecomposition object
+     * @brief Size of the matrix (as matrix is square, this size is rows' size and columns' size)
      * 
      */
-    LUDecomposition() {
-        isEmpty = true;
+    std::size_t size;
+
+    /**
+     * @brief Sets to true, if decomposition wasn't gotten
+     * 
+     */
+	bool empty;
+};
+
+template<class T>
+LUDecomposition<T>::LUDecomposition() {
+    empty = true;
+    size = 0;
+}
+
+template<class T>
+LUDecomposition<T>::LUDecomposition(const Matrix<T>& matrix) {
+    decompose(matrix);
+}
+
+template<class T>
+bool LUDecomposition<T>::decompose(const Matrix<T>& matrix) {
+    if (!matrix.isSquare()) {
         size = 0;
+        empty = true;
+        return !empty;
     }
 
-    /**
-     * @brief Construct a new LUDecomposition object with decomposition of given matrix
-     * 
-     * @param Matrix to get decomposition
-     */
-	LUDecomposition(const Matrix<T>& matrix) {
-        decompose(matrix);
-	}
+    size = matrix.getRows();
+    empty = false;
 
-    /**
-     * @brief Decomposes given matrix
-     * 
-     * @param Matrix to get decomposition
-     * @return true if decomposition was successfully gotten
-     * @return false if decomposition wasn't gotten
-     */
-    bool decompose(const Matrix<T>& matrix) {
-        if (!matrix.isSquare()) {
-            size = 0;
-            isEmpty = true;
-            return !isEmpty;
+    RawMatrix<T> lower = Matrix<T>::allocateRawMatrix(size, size);
+    RawMatrix<T> upper = Matrix<T>::allocateRawMatrix(size, size);
+
+    for (std::size_t i = 0; i < size; ++i) {
+
+        // Upper Triangular
+        for (std::size_t k = i; k < size; ++k) {
+
+            // Summation of L(i, j) * U(j, k)
+            int sum = 0;
+            for (std::size_t j = 0; j < i; ++j)
+                sum += (lower[i][j] * upper[j][k]);
+
+            // Evaluating U(i, k)
+            upper[i][k] = matrix.get(i, k) - sum;
         }
 
-        size = matrix.getRows();
-        isEmpty = false;
-
-        RawMatrix<T> lower = Matrix<T>::allocateRawMatrix(size, size);
-        RawMatrix<T> upper = Matrix<T>::allocateRawMatrix(size, size);
-
-        for (size_t i = 0; i < size; ++i) {
-
-            // Upper Triangular
-            for (size_t k = i; k < size; ++k) {
-
-                // Summation of L(i, j) * U(j, k)
+        // Lower Triangular
+        for (std::size_t k = i; k < size; ++k) {
+            if (i == k)
+                lower[i][i] = 1; // Diagonal as 1
+            else {
+                // Summation of L(k, j) * U(j, i)
                 int sum = 0;
                 for (size_t j = 0; j < i; ++j)
-                    sum += (lower[i][j] * upper[j][k]);
+                    sum += (lower[k][j] * upper[j][i]);
 
-                // Evaluating U(i, k)
-                upper[i][k] = matrix.get(i, k) - sum;
-            }
-
-            // Lower Triangular
-            for (size_t k = i; k < size; ++k) {
-                if (i == k)
-                    lower[i][i] = 1; // Diagonal as 1
-                else {
-                    // Summation of L(k, j) * U(j, i)
-                    int sum = 0;
-                    for (size_t j = 0; j < i; ++j)
-                        sum += (lower[k][j] * upper[j][i]);
-
-                    // Evaluating L(k, i)
-                    lower[k][i] = (matrix.get(k, i) - sum) / upper[i][i];
-                }
+                // Evaluating L(k, i)
+                lower[k][i] = (matrix.get(k, i) - sum) / upper[i][i];
             }
         }
-
-        L = std::make_shared<Matrix<T>>(lower);
-        U = std::make_shared<Matrix<T>>(upper);
-
-        return !isEmpty;
     }
-};
+
+    L = std::make_shared<Matrix<T>>(lower);
+    U = std::make_shared<Matrix<T>>(upper);
+
+    return !empty;
+}
+
+template<class T>
+std::shared_ptr<Matrix<T>> LUDecomposition<T>::getL() const {
+    return L;
+}
+
+template<class T>
+std::shared_ptr<Matrix<T>> LUDecomposition<T>::getU() const {
+    return U;
+}
+
+template<class T>
+std::size_t LUDecomposition<T>::getSize() const {
+    return size;
+}
+
+template<class T>
+bool LUDecomposition<T>::isEmpty() const {
+    return empty;
+}
 
 }
